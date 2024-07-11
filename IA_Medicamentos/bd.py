0c723aba075962ea7b0bd9e650ee1093b904436e
MoiB03 <102995807+Moisinho@users.noreply.github.com>
2024-07-11 11:40:50 -0500

    Subiendo archivos pesados

---
+++ IA_Medicamentos/bd.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:af9bd9720eb58c201938f6c07b29c4c17b1a800b0a4107c46d065c99092a77a5
+size 1580
---
+++ IA_Medicamentos/main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:db6c67b6768467181de6ba434d1e0d190c97755f90f902c5a80e3bad5de82937
+size 6501
