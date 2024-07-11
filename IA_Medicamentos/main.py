ca7fe580d626a387c27ab3a41ba11b7c6892bf51
MoiB03 <102995807+Moisinho@users.noreply.github.com>
2024-07-11 14:42:44 -0500

    subiendo cambios

--- IA_Medicamentos/main.py
+++ IA_Medicamentos/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db6c67b6768467181de6ba434d1e0d190c97755f90f902c5a80e3bad5de82937
-size 6501
+oid sha256:feb01122a42a9701b4c72ce1d94dda7810600cf436532fbabe2e7d431117d3ed
+size 7276
---
+++ IA_Medicamentos/prueba.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c7dfc3f4b9da3a1ca5402d6f168a170e03670fe31341004580e945e87657455e
+size 642
